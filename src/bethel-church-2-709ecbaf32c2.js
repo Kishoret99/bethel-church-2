127afcee7d02244532df72e4a75b495cb98aa1fd
KishoreTorlakonda Jum <kishore.torlakonda@jumbaya.com>
2023-07-08 21:37:06 +0530

    working example

---
+++ src/bethel-church-2-709ecbaf32c2.js
@@ -0,0 +1,16 @@
+const serviceJson = {
+  "type": "service_account",
+  "project_id": "bethel-church-2",
+  "private_key_id": "709ecbaf32c2e918c1190b0459ac347e693d76e6",
+  "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQDX/JN6zJCjIniE\nKiW4YKWt9X+OVRkPV/rAZ9yjAkPTNS80BQoGpDAcpg3SpS6SRMBIs1vagSCVq4tP\n4eQghRHeemzKNhArOKyjiPyH8m9km01ErL5wQhB4gUEnwPzwT++2x9bouc5zPd8G\nFWlVWYSsCctwDIP/DGWDSasBtX6t79wBomXCT+xf5AROXzNZ8iXiXccmEwxs2agU\nPLpWNtY5zmsdUBGrPjnxToFki2ZbeXoozLrF5d+DhXxs2bsWhFjDMWlCiOo7l8OI\n328fZ6qTqTA17l0MefJWPTFZ/hT0KJE4jl7BLcyV3nnMJiki7rTKCe3QYfCDOy16\n01/uoTr9AgMBAAECggEAKyUZwtCFXgkfQOt4cH3h6r+dNO373QxKm0/EC84ChXjc\nzovpJk17DOYA4eC8TlysenlVsm7T9wH3CMSLgRGD8cjL3ZLyJOgj2GEfbPq5VMQP\nKdZGDn+r8Z6ESPAstsaJEJ6X6BJh0rR7zifNzE5fCczFFkNxxKuRxamcHvHF9fGT\niIMDE3NjKVdEJn5Ryj7zuR8x4388P4UGDz+YnGIcBwHbOKbFBH9bgkpOOdpWPre9\ndGJ+aR8pK8EZfRTKuarsTZtd4HgbeQdoq9i67CIx+itpC4hc5QVmDpvM+Dya+K2D\n7MvFbqb3JPoCLBj2MgTkALYNrcoXEAW3riERuu9zkwKBgQD5sh3OkkEnIr5Uk8Tq\nRABi/n2IAyeaXiUtBXUkrtqyTd9EOEr+7L1pS5J4YMGmN5qypnkK+qy2XBR8uMo8\n7/RzZulEflPUEB1wqpHgRmRmgUw5LGCYoJQphbedHR00lm9tdBh+TQKih1RkdcT1\nI5KlO+BZmopH4pZqei5DCRWR8wKBgQDdcJWA72uiJz9VIwZ01oibTN3Z2WbIJtSE\n/HawjTYFLex37dRR8wyNduUZho8JJXvZT6lomlDOcQy3vRbb4IEr/cpRw9OMjRjZ\nek1i8zu31AjqMOZK7jlIBv5+bICazfogrnUmFueT2UT7540MelZXMR7Eces8XuKy\ne1h9mq1LTwKBgQD1P99K8BgSSmvLQsCcoGII2ZGOsBqL4oB9MekXOK8Rhq8OoIP4\n06pO661XkyFhEyhc5GnrQL+29cYhopbxN3ZQivn/Dl1j4tgnu0FAmaMvkzu8xDu0\nbbRY/D+Wj+wJpOCZDvinxRgbbvSTH6FubZwxsxfuqn27kXDP8GXT7e5DxQKBgFTD\nCZcnDi4tk8KE+62xiOzZSXpgC0XGB0c4VD5Ubz8gSwnFEaakwENOJXfV196KaJPQ\nOGp/1zqOb23NhXh4wTkTlmQh71I3mDF27TRZIpzFHvx151D7SxXVB4lhAwE+ouXr\noZAI6G/hlNRP19/U3EPX240ahg6/y0rCiCbcFEVlAoGBAObQGJu0MMv11ysGALM7\nVBMzgiZVEP6a9abuVtRU5caVQ9BzXn6lkc7TqmyPMY2SFFXxmiYFRUGvngQb7FGf\nJNhNhNTGkDAiAd8GcX+Ubq1JFzK3E/Ut2DCRGdQiLtv3bDaK1svw9y2Xv371HwxH\n7Llqr0aCJV4vqD/tE5lANiMO\n-----END PRIVATE KEY-----\n",
+  "client_email": "app-350@bethel-church-2.iam.gserviceaccount.com",
+  "client_id": "106784773180702526793",
+  "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+  "token_uri": "https://oauth2.googleapis.com/token",
+  "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+  "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/app-350%40bethel-church-2.iam.gserviceaccount.com",
+  "universe_domain": "googleapis.com",
+  "sheetId": "1rU44kodCfJOp0Zug3B5i6lr94E8Y24k1GH7v66OH2zw",
+  "apiKey": "AIzaSyCdy7Zu9tI9NaVuF9-C5qzGUeIMWGG2khk"
+}
+export default serviceJson;
